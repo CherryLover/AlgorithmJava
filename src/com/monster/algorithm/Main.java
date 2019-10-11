2d2fdeb42e42578f0b34b1f50b84b6855bab4847
jiangjiwei <jiangjiwei0322@outlook.com>
2019-10-11 10:16:30 +0800

    1. 实现数组的插入操作，无自动扩容

--- src/com/monster/algorithm/Main.java
+++ src/com/monster/algorithm/Main.java
@@ -1,7 +1,17 @@
 package com.monster.algorithm;
 
+import com.monster.algorithm.structure.MyArray;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Algorithm World");
+        MyArray myArray = new MyArray(10);
+        myArray.insert(0, 5);
+        myArray.insert(1, 8);
+        myArray.insert(0,3);
+//        myArray.insert(2, 10);
+//        myArray.insert(0, 3);
+//        myArray.insert(2, 2);
+        myArray.out();
     }
 }
---
+++ src/com/monster/algorithm/structure/MyArray.java
@@ -0,0 +1,35 @@
+package com.monster.algorithm.structure;
+
+import static com.monster.algorithm.Utils.printArray;
+
+public class MyArray {
+    private int[] array;
+    private int size;
+
+    public MyArray(int capacity) {
+        array = new int[capacity];
+        this.size = 0;
+    }
+
+    /**
+     * 1. 插入新数据时，向右移动原有数据为插入元素腾空位置，后再执行插入
+     * @param index 下标
+     * @param element 元素
+     */
+    public void insert(int index, int element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("超出数组实际长度");
+        }
+        // 复制待插入位置及其后的数据
+        for (int i = size - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        // 插入新的数据
+        array[index] = element;
+        size++;
+    }
+
+    public void out() {
+        printArray(array);
+    }
+}
