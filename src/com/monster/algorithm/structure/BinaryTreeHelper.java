075380a2c02c9db60c6cc5eb099a828dacb9c1a0
jiangjiwei <jiangjiwei0322@outlook.com>
2019-10-16 10:48:15 +0800

    new structure 1. add BinaryTree and 3 kinds order

---
+++ src/com/monster/algorithm/structure/BinaryTreeHelper.java
@@ -0,0 +1,92 @@
+package com.monster.algorithm.structure;
+
+import java.util.LinkedList;
+
+public class BinaryTreeHelper {
+
+    private BinaryTreeHelper() {
+    }
+
+    public static BinaryTreeHelper getInstance() {
+        return Holder.single;
+    }
+
+    /**
+     * 利用递归调用不断的调用当前方法去二叉树节点
+     */
+    public TreeNode createBinaryTree(LinkedList<Integer> linkedList) {
+        if (linkedList == null || linkedList.isEmpty()) {
+            return null;
+        }
+        TreeNode node = null;
+        Integer data = linkedList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.left = createBinaryTree(linkedList);
+            node.right = createBinaryTree(linkedList);
+        }
+        return node;
+    }
+
+    /**
+     * 二叉树的前序调用
+     *
+     * @param node 节点
+     * @return 节点值
+     */
+    public String preOrder(TreeNode node) {
+        StringBuilder sb = new StringBuilder();
+        if (node != null) {
+            sb.append(node.data).append(",");
+            sb.append(preOrder(node.left));
+            sb.append(preOrder(node.right));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 二叉树的中序调用
+     *
+     * @param node 节点
+     * @return 节点值
+     */
+    public String middleOrder(TreeNode node) {
+        StringBuilder sb = new StringBuilder();
+        if (node != null) {
+            sb.append(middleOrder(node.left));
+            sb.append(node.data).append(",");
+            sb.append(middleOrder(node.right));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 二叉树的后序节点
+     *
+     * @param node 节点
+     * @return 节点值
+     */
+    public String postOrder(TreeNode node) {
+        StringBuilder sb = new StringBuilder();
+        if (node != null) {
+            sb.append(postOrder(node.left));
+            sb.append(postOrder(node.right));
+            sb.append(node.data).append(",");
+        }
+        return sb.toString();
+    }
+
+    private static class Holder {
+        private static final BinaryTreeHelper single = new BinaryTreeHelper();
+    }
+
+    class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+}
---
+++ src/com/monster/algorithm/structure/BinaryTreeHelperTest.java
@@ -0,0 +1,46 @@
+package com.monster.algorithm.structure;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeHelperTest {
+
+    @Test
+    public void test_create_binaryTree() {
+        Integer[] sampleArray = {3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        BinaryTreeHelper.TreeNode binaryTree = BinaryTreeHelper.getInstance().createBinaryTree(new LinkedList<>(Arrays.asList(sampleArray)));
+        assertEquals(binaryTree.data, 3);
+    }
+
+    @Test
+    public void test_binaryTree_preOrder() {
+        Integer[] sampleArray = {3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        BinaryTreeHelper instance = BinaryTreeHelper.getInstance();
+        BinaryTreeHelper.TreeNode binaryTree = instance.createBinaryTree(new LinkedList<>(Arrays.asList(sampleArray)));
+        String out = instance.preOrder(binaryTree);
+        assertEquals(out, "3,2,9,10,8,4,");
+    }
+
+    @Test
+    public void test_binaryTree_middleOrder() {
+        Integer[] sampleArray = {3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        BinaryTreeHelper instance = BinaryTreeHelper.getInstance();
+        BinaryTreeHelper.TreeNode binaryTree = instance.createBinaryTree(new LinkedList<>(Arrays.asList(sampleArray)));
+        String out = instance.middleOrder(binaryTree);
+        assertEquals(out, "9,2,10,3,8,4,");
+    }
+
+    @Test
+    public void test_binaryTree_postOrder() {
+        Integer[] sampleArray = {3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        BinaryTreeHelper instance = BinaryTreeHelper.getInstance();
+        BinaryTreeHelper.TreeNode binaryTree = instance.createBinaryTree(new LinkedList<>(Arrays.asList(sampleArray)));
+        String out = instance.postOrder(binaryTree);
+        assertEquals(out, "9,10,2,4,8,3,");
+    }
+
+}
