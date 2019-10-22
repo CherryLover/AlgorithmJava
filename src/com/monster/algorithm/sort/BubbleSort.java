db15b87613e79f6382f51b9ff1eeb29e79637962
jiangjiwei <jiangjiwei0322@outlook.com>
2019-10-22 14:55:29 +0800

    new test 1. better for bubble sort

--- src/com/monster/algorithm/sort/BubbleSort.java
+++ src/com/monster/algorithm/sort/BubbleSort.java
@@ -4,17 +4,25 @@ import com.monster.algorithm.Utils;
 
 public class BubbleSort implements Sort {
 
+    private int sortCount = 0;
     @Override
     public String sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+            boolean isSorted = true;
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                    isSorted = false;
                 }
             }
+            if (isSorted) {
+                break;
+            }
+            sortCount++;
         }
+        System.out.println("排序次数：" + sortCount);
         return Utils.getStringFromArray(array);
     }
 }
