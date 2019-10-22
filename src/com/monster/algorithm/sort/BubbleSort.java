f2b3a13bda6456fb2f3e98d07cea0aac0270d089
jiangjiwei <jiangjiwei0322@outlook.com>
2019-10-22 15:05:38 +0800

    new test 1. better for bubble sort

--- src/com/monster/algorithm/sort/BubbleSort.java
+++ src/com/monster/algorithm/sort/BubbleSort.java
@@ -7,16 +7,21 @@ public class BubbleSort implements Sort {
     private int sortCount = 0;
     @Override
     public String sort(int[] array) {
+        int lastExchangeIndex = 0;
+        int sortBorder = array.length - 1;
+
         for (int i = 0; i < array.length; i++) {
             boolean isSorted = true;
-            for (int j = 0; j < array.length - 1; j++) {
+            for (int j = 0; j < sortBorder; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                     isSorted = false;
+                    lastExchangeIndex = j;
                 }
             }
+            sortBorder = lastExchangeIndex;
             if (isSorted) {
                 break;
             }
--- src/com/monster/algorithm/sort/SortTest.java
+++ src/com/monster/algorithm/sort/SortTest.java
@@ -13,4 +13,11 @@ public class SortTest {
         Sort sort = new BubbleSort();
         assertEquals("1,2,3,5,6,7,8,9", sort.sort(array));
     }
+
+    @Test
+    public void test_bubble_sort_better() {
+        int[] array = {3, 4, 2, 1, 5, 6, 7, 8};
+        Sort sort = new BubbleSort();
+        assertEquals("1,2,3,4,5,6,7,8", sort.sort(array));
+    }
 }
